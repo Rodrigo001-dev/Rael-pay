1aabcbad89d576f4c4837630315aaafc05afdfe3
Rodrigo <rodrigorael53@gmail.com>
2021-03-06 00:06:20 -0300

    using tasks(async, await)

--- lib/raelPay_web/controllers/accounts_Controller.ex
+++ lib/raelPay_web/controllers/accounts_Controller.ex
@@ -1,3 +1,13 @@
+# cada request que um controller recebe em elixir é um processo executado
+# separadamente
+# um processo do sistema operacional normalmente é caro de ser criado então a
+# maquina virtual está rodando sobre um processo só que a
+# bin(a maquina virtual do erlang) ela tem seus próprios processo que são leves,
+# rápidos, consomem pouca memória e eles podem ser criados aos milhares, então
+# cada request que é feito para a nossa aplicação é um novo processo do bin
+# que é criado, então mesmo que um processo se quebre, de exceção, que dê um erro,
+# qualquer coisa desse tipo não vai afetar os outros processos
+
 defmodule RaelPayWeb.AccountsController do
   use RaelPayWeb, :controller
 
@@ -23,7 +33,10 @@ defmodule RaelPayWeb.AccountsController do
   end
 
   def transaction(conn, params) do
-    with {:ok, %TransactionResponse{} = transaction} <- RaelPay.transaction(params) do
+    # utilizando tasks
+    task = Task.async(fn -> RaelPay.transaction(params) end)
+
+    with {:ok, %TransactionResponse{} = transaction} <- Task.await(task) do
       conn
       |> put_status(:ok)
       |> render("transaction.json", transaction: transaction)
